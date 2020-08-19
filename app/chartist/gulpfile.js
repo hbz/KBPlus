d5242afa75d59059a81479d4d273039cb923562c
Ingrid Bluoss <ingrid.bluoss@hbz-nrw.de>
2020-08-19 12:13:40 +0200

    Aktualisieren der gulp-Prozesse für semantic und chartist: Erneuerung der semantic tasks von 2.3.1 auf 2.4.2

--- app/chartist/gulpfile.js
+++ app/chartist/gulpfile.js
@@ -4,7 +4,7 @@ var postcss = require('gulp-postcss');
 sass.compiler = require('node-sass');
 
 gulp.task('build', function () {
-    return gulp.src('./src/chartist.scss')
+    return gulp.src(['./src/chartist.scss','./src/chartist-plugin-legend.scss'])
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'))
         .pipe(postcss())
